337b3885bf351241bbd607c4b53c8e9db80e763a
zihuv <zihuv@qq.com>
2023-01-18 22:27:26 +0800

    测试hello 第5次修改 master

---
+++ src/main/java/com/zihuv/Day01181817Application.java
@@ -0,0 +1,13 @@
+package com.zihuv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01181817Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day01181817Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zihuv/Day01181817ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zihuv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01181817ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
