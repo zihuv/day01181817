5b25eb4098a2fdbe1b5d35ded1bc3857097d9802
zihuv <zihuv@qq.com>
2023-01-18 22:21:00 +0800

    测试hello 第4次修改 hot-fix

--- src/main/java/com/zihuv/hello.java
+++ src/main/java/com/zihuv/hello.java
@@ -2,8 +2,6 @@ package com.zihuv;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("merge hot-fix");
+        System.out.println("hot-fix");
     }
 }
